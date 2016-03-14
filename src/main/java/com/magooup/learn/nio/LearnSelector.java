acbdf6ac70152fe8e2ff4222e68ed4949586cfea
magooup <magoo.up@gmail.com>
2016-03-14 17:34:42 +0800

    Reviewing

--- src/main/java/com/magooup/learn/nio/LearnSelector.java
+++ src/main/java/com/magooup/learn/nio/LearnSelector.java
@@ -2,9 +2,12 @@ package com.magooup.learn.nio;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
 
 /**
  * Created by zhiyong.ma on 2016/3/11.
@@ -21,40 +24,127 @@ public class LearnSelector {
         }
     }
 
-    public static void main(String[] args) {
+    static {
+        Server.start();
+    }
 
+    public static void main(String[] args) throws IOException {
+        selector();
     }
 
     static void selector() throws IOException {
 
+        SocketChannel channel1 = SocketChannel.open(new InetSocketAddress("127.0.0.1", 8888));
+        channel1.configureBlocking(false);
+        channel1.register(selector, SelectionKey.OP_WRITE);
+        SocketChannel channel2 = SocketChannel.open(new InetSocketAddress("127.0.0.1", 9999));
+        channel2.configureBlocking(false);
+        channel2.register(selector, SelectionKey.OP_WRITE);
+        while (true) {
+            int num = selector.select(1000);
+            System.out.println("Ready " + num);
+            if (num > 0) {
+                Set<SelectionKey> set = selector.selectedKeys();
+                for (final SelectionKey key : set) {
 
+                    if (key.isWritable()) {
+                        new Thread() {
+                            @Override
+                            public void run() {
+                                try {
+                                    ByteBuffer buffer = ByteBuffer.allocate(1024);
+                                    SocketChannel channel = (SocketChannel) key.channel();
+                                    buffer.put((channel + "").getBytes());
+                                    buffer.flip();
+                                    while (buffer.hasRemaining()) {
+                                        System.out.println("Do a write: " + channel);
+                                        channel.write(buffer);
+                                    }
+                                    buffer.clear();
+                                    while (true) {
+                                        while (-1 != (channel.read(buffer))) {
+                                            buffer.flip();
+                                            if (buffer.limit() <= 0) {
+                                                continue;
+                                            }
+                                            byte[] bytes = new byte[buffer.limit()];
+                                            buffer.get(bytes);
+                                            System.out.println("Accept a response: " + new String(bytes));
+                                        }
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }.start();
+                    }
+
+                    if (key.isAcceptable()) {
+                        new Thread() {
+                            @Override
+                            public void run() {
+                                try {
+                                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                                    SocketChannel channel = null;
+                                    if (null != (channel = server.accept())) {
+                                        ByteBuffer buffer = ByteBuffer.allocate(1024);
+                                        buffer.clear();
+                                        while (-1 != channel.read(buffer)) {
+                                            buffer.flip();
+                                            while (buffer.hasRemaining()) {
+                                                byte[] bytes = new byte[buffer.limit()];
+                                                buffer.get(bytes);
+                                                System.out.println("Do a read: " + new String(bytes));
+                                            }
+                                            buffer.clear();
+                                        }
+                                        buffer.clear();
+                                        buffer.put((server + "").getBytes());
+                                        System.out.println("Write a response: " + server);
+                                        buffer.flip();
+                                        channel.write(buffer);
+                                        buffer.clear();
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }.start();
+                    }
+                }
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
+
     static class Server {
-        void start() {
+        static void start() {
             Thread damon = new Thread() {
                 @Override
                 public void run() {
-                    ServerSocket server = null;
                     try {
-                        server = new ServerSocket();
-                        server.bind(new InetSocketAddress("127.0.0.1", 8888));
-                        setDaemon(true);
-                        while (true) {
-                            try {
-                                Socket socket = null;
-                                while (null != (socket = server.accept())) {
+                        ServerSocketChannel server1 = ServerSocketChannel.open();
+                        server1.configureBlocking(false);
+                        server1.bind(new InetSocketAddress("127.0.0.1", 8888));
+                        System.out.println("Server1 started, listening at 127.0.0.1:8888");
+                        ServerSocketChannel server2 = ServerSocketChannel.open();
+                        server2.configureBlocking(false);
+                        server2.bind(new InetSocketAddress("127.0.0.1", 9999));
+                        System.out.println("Server2 started, listening at 127.0.0.1:9999");
 
-                                }
-                            } catch (IOException e) {
-
-                            }
-                        }
+                        server1.register(selector, SelectionKey.OP_ACCEPT);
+                        server2.register(selector, SelectionKey.OP_ACCEPT);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             };
+            damon.start();
         }
     }
 
